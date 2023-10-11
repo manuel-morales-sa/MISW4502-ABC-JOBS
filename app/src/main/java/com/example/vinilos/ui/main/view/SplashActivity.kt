514f3c7719904357f59c539ff4ff5b20b82ee8eb
Manuel Morales <mf.moraless1@uniandes.edu.co>
2023-10-11 15:39:38 -0500

    add splash activity

--- app/src/main/java/com/example/vinilos/ui/main/view/SplashActivity.kt
+++ app/src/main/java/com/example/vinilos/ui/main/view/SplashActivity.kt
@@ -2,9 +2,11 @@ package com.example.vinilos.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vinylsMobile.vinylsapplication.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Vinilos)
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_splash)
     }
